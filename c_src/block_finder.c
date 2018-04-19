bb474d4028713b50733b3d972b1865f7c6dfb409
Stefan Bodewig <stefan.bodewig@innoq.com>
2018-04-19 16:17:02 +0200

    implement SHA256 calculation with hard coded values

--- c_src/block_finder.c
+++ c_src/block_finder.c
@@ -1,12 +1,37 @@
+#include <stdlib.h>
 #include <string.h>
 #include <erl_nif.h>
+#include <openssl/sha.h>
 
-static ERL_NIF_TERM find_block_parts_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-  ERL_NIF_TERM result;
-  unsigned char *data = enif_make_new_binary(env, 18, &result);
-  memcpy(data, "Hello world from C", 18);
-  return enif_make_tuple(env, 3, enif_make_atom(env, "true"), result, result);
+static ERL_NIF_TERM find_block_parts_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+  ErlNifBinary prefix, suffix;
+
+  if (enif_inspect_binary(env, argv[0], &prefix)
+      && enif_inspect_binary(env, argv[1], &suffix)) {
+    unsigned char byteResultHash[SHA256_DIGEST_LENGTH];
+    unsigned char proof[11];
+    SHA256_CTX c;
+
+    snprintf(proof, 11, "%d", 1917336);
+    int proof_len = strlen(proof);
+
+    SHA256_Init(&c);
+    SHA256_Update(&c, prefix.data, prefix.size);
+    SHA256_Update(&c, &proof, proof_len);
+    SHA256_Update(&c, suffix.data, suffix.size);
+
+    //Finalize and get the hash data
+    SHA256_Final(byteResultHash, &c);
+
+    ERL_NIF_TERM proof_term, hash_term;
+
+    unsigned char *proof_data = enif_make_new_binary(env, proof_len, &proof_term);
+    memcpy(proof_data, &proof, proof_len);
+    unsigned char *hash_data = enif_make_new_binary(env, SHA256_DIGEST_LENGTH, &hash_term);
+    memcpy(hash_data, &byteResultHash, SHA256_DIGEST_LENGTH);
+    return enif_make_tuple(env, 3, enif_make_atom(env, "true"), proof_term, hash_term);
+  }
+  return enif_make_tuple(env, 1, enif_make_atom(env, "false"));
 }
 
 static ErlNifFunc nif_funcs[] = {
